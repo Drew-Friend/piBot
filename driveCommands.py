c0ef426c7a53a7e6343fbf2979629e7768d96bb0
Drew-Friend <dfriend414@gmail.com>
2021-09-20 19:30:59 -0400

    debugging

--- driveCommands.py
+++ driveCommands.py
@@ -4,14 +4,16 @@ from adafruit_motorkit import MotorKit
 class DriveTrain:
     def __innit__(self):
         self.kit = MotorKit(0x40)
+        self.m1 = self.kit.motor1
+        self.m2 = self.kit.motor2
 
     def arcade(self, x, y):
         pass
 
     def tank(self, p1, p2):
-        self.kit.motor1.throttle = p1
-        self.kit.motor2.throttle = p2
+        self.m1.throttle = p1
+        self.m2.throttle = p2
 
     def tank(self, power):
-        self.kit.motor1.throttle = power
-        self.kit.motor2.throttle = power
+        self.m1.throttle = power
+        self.m2.throttle = power
