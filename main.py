c8e754acd137290d9a947f2538c1a3452e850ac2
Drew-Friend <dfriend414@gmail.com>
2021-09-24 14:19:26 -0400

    Made branch for minibot, added a connection check to periodic, which I will soon add to the main branch

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 # Python imports and my classes
 import time
 import board
-from subsystems import driveCommands, controller, leds, relays
+from subsystems import driveCommands, controller, relays
 
 # Instantiate the subsystems used on the robot
-drive = driveCommands.analogDrive()
+drive = driveCommands.digitalDrive()
 controls = controller.emptyTester()
-backLight = leds.Strand(board.D21, 15)
 spinner = relays.Pair(board.D6, board.D13)
 spinnerPrev = False
-boostPrev = False
 
 
 def periodic():
     global spinnerPrev
     global boostPrev
-    controls.readOutput()
+    try:
+        controls.readOutput()
+    except:
+        return False
     drive.arcade(controls.throttle, controls.turn)
-
-    if controls.b and not boostPrev:
-        drive.toggle_SUPER_ULTRA_MEGA_GOD_MODE()
-        boostPrev = True
-    if boostPrev and not controls.b:
-        boostPrev = False
 
     if controls.a and not spinnerPrev:
         spinner.toggle()
@@ -32,9 +27,10 @@ def periodic():
 
     print(
         "Motor 1: {}, Motor 2: {}, Spinner: {}".f(
-            drive.m1, drive.m2, spinner.motor2.port.value
+            drive.left, drive.right, spinner.motor2.port.value
         )
     )
+    return True
 
 
 connected = False
@@ -43,14 +39,8 @@ while True:
     drive.off()
     spinner.off()
     while not connected:
-        backLight.bounce()
-        # For loop makes it so LEDs don't update on every loop, so the animation is slower
-        for i in range(10):
-            # Bluetooth search function
-            pass
+        # Bluetooth search function
+        pass
 
     while connected:
-        backLight.rotate()
-        # For loop makes it so LEDs don't update on every loop, so the animation is slower
-        for i in range(10):
-            periodic()
+        connected = periodic()
