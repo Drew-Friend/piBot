c492a9461427ef9fb324bd851844e0844b610a49
Drew-Friend <dfriend414@gmail.com>
2021-09-27 13:34:13 -0400

    bluetooth

--- subsystems/ble.py
+++ subsystems/ble.py
@@ -6,29 +6,33 @@ class btComm:
         self.addr = addr
         self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
-    # def scan(self):
-    #     nearby_devices = bluetooth.discover_devices()
-    #     target_address = None
-    #     for bdaddr in nearby_devices:
-    #         if self.addr == bdaddr:
-    #             target_address = bdaddr
-    #             break
     def scan(self):
-        print("Scanning for bluetooth devices:")
-        devices = bluetooth.discover_devices(lookup_names=True, lookup_class=True)
-        number_of_devices = len(devices)
-        print(number_of_devices, "devices found")
-        for addr, name, device_class in devices:
-            print("\n")
-            print("Device:")
-            print("Device Name: %s" % (name))
-            print("Device MAC Address: %s" % (addr))
-            print("Device Class: %s" % (device_class))
-            print("\n")
-            if addr == self.addr:
+        nearby_devices = bluetooth.discover_devices()
+        target_address = None
+        for bdaddr in nearby_devices:
+            if self.addr == bdaddr:
+                print(bdaddr)
                 self.sock.connect((self.addr, self.port))
                 self.sock.send("\x1A")
-                print("Connected to %s" % (name))
+                print("Connected to %s" % (bdaddr))
+                break
+
+    # def scan(self):
+    #     print("Scanning for bluetooth devices:")
+    #     devices = bluetooth.discover_devices(lookup_names=True, lookup_class=True)
+    #     number_of_devices = len(devices)
+    #     print(number_of_devices, "devices found")
+    #     for addr, name, device_class in devices:
+    #         print("\n")
+    #         print("Device:")
+    #         print("Device Name: %s" % (name))
+    #         print("Device MAC Address: %s" % (addr))
+    #         print("Device Class: %s" % (device_class))
+    #         print("\n")
+    #         if addr == self.addr:
+    #             self.sock.connect((self.addr, self.port))
+    #             self.sock.send("\x1A")
+    #             print("Connected to %s" % (name))
 
     def read(self):
         return self.sock.getOutputStream()
